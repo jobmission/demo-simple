d8e1230c0221c98e498e78d34c99b0c52b3ab8af
jobmission <contact@revengemission.com>
2020-01-14 17:54:22 +0800

    update fastdfs test

--- src/test/java/com/example/demo/DfsTest.java
+++ src/test/java/com/example/demo/DfsTest.java
@@ -1,26 +1,41 @@
 package com.example.demo;
 
 import org.csource.common.MyException;
+import org.csource.common.NameValuePair;
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.FileInfo;
+import org.csource.fastdfs.StorageClient1;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.*;
+import java.util.Properties;
 
 public class DfsTest {
+
+    String trackerServers = "192.168.1.6:22122";
 
     @Ignore
     @Test
     public void uploadTest() {
+        Properties properties = new Properties();
+        properties.put(ClientGlobal.PROP_KEY_TRACKER_SERVERS, trackerServers);
         try {
-            FastDFSClient fastDFSClient = new FastDFSClient();
+            FastDFSClient fastDFSClient = new FastDFSClient(properties);
             File file = new File("E:\\test.txt");
+            String fileName = file.getName();
+            String suffix = fileName.substring(fileName.lastIndexOf(".") + 1);
             byte[] bytes = toByteArray(file);
-            System.out.println("newName: " + fastDFSClient.getStorageClient1("group1").upload_file1(bytes, "txt", null));
-            System.out.println("newName: " + fastDFSClient.getStorageClient1("group1").upload_file1(bytes, "txt", null));
-            System.out.println("newName: " + fastDFSClient.getStorageClient1("group1").upload_file1(bytes, "txt", null));
-            System.out.println("newName: " + fastDFSClient.getStorageClient1("group1").upload_file1(bytes, "txt", null));
-            System.out.println("newName: " + fastDFSClient.getStorageClient1("group2").upload_file1(bytes, "txt", null));
-            System.out.println("newName: " + fastDFSClient.getStorageClient1("group3").upload_file1(bytes, "txt", null));
+            NameValuePair[] metaList = new NameValuePair[2];
+            //这是个数组，可以继续添加
+            metaList[0] = new NameValuePair("fileName", fileName);
+            metaList[1] = new NameValuePair("category", "advertisement");
+            System.out.println("newName: " + fastDFSClient.getStorageClient1("group1").upload_file1(bytes, suffix, metaList));
+            System.out.println("newName: " + fastDFSClient.getStorageClient1("group1").upload_file1(bytes, suffix, metaList));
+            System.out.println("newName: " + fastDFSClient.getStorageClient1("group1").upload_file1(bytes, suffix, metaList));
+            System.out.println("newName: " + fastDFSClient.getStorageClient1("group1").upload_file1(bytes, suffix, metaList));
+            System.out.println("newName: " + fastDFSClient.getStorageClient1("group2").upload_file1(bytes, suffix, metaList));
+            System.out.println("newName: " + fastDFSClient.getStorageClient1("group3").upload_file1(bytes, suffix, metaList));
         } catch (IOException e) {
             e.printStackTrace();
         } catch (MyException e) {
@@ -29,6 +44,50 @@ public class DfsTest {
             e.printStackTrace();
         }
 
+    }
+
+    @Ignore
+    @Test
+    public void testQuery() {
+        Properties properties = new Properties();
+        properties.put(ClientGlobal.PROP_KEY_TRACKER_SERVERS, trackerServers);
+        try {
+            FastDFSClient fastDFSClient = new FastDFSClient(properties);
+            //定义storage的客户端
+            StorageClient1 client = fastDFSClient.getStorageClient1("group2");
+            FileInfo fileInfo1 = client.query_file_info("group2", "M00/00/00/CgkQTl4dd3qANg1dAAAy_nv9wkc515.txt");
+            FileInfo fileInfo2 = client.query_file_info1("group2/M00/00/00/CgkQTl4dd3qANg1dAAAy_nv9wkc515.txt");
+            System.out.println(fileInfo1);
+            System.out.println(fileInfo2);
+            //查询文件元信息
+            NameValuePair[] metadata1 = client.get_metadata1("group2/M00/00/00/CgkQTl4dd3qANg1dAAAy_nv9wkc515.txt");
+            for (NameValuePair temp : metadata1) {
+                System.out.println(temp.getName() + "\t" + temp.getValue());
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    @Ignore
+    @Test
+    public void testDownLoad() {
+        Properties properties = new Properties();
+        properties.put(ClientGlobal.PROP_KEY_TRACKER_SERVERS, trackerServers);
+        try {
+            FastDFSClient fastDFSClient = new FastDFSClient(properties);
+            //定义storage的客户端
+            StorageClient1 client = fastDFSClient.getStorageClient1("group2");
+            //下载
+            byte[] bytes = client.download_file1("group2/M00/00/00/CgkQTl4dd3qANg1dAAAy_nv9wkc515.txt");
+            File file = new File("e:/456.txt");
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(bytes);
+            fos.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     /*读取文件的字节数组*/
--- src/test/java/com/example/demo/FastDFSClient.java
+++ src/test/java/com/example/demo/FastDFSClient.java
@@ -1,20 +1,19 @@
 package com.example.demo;
 
-import org.csource.fastdfs.ClientGlobal;
-import org.csource.fastdfs.StorageClient1;
-import org.csource.fastdfs.TrackerClient;
-import org.csource.fastdfs.TrackerServer;
+import org.csource.common.MyException;
+import org.csource.fastdfs.*;
+
+import java.io.IOException;
+import java.util.Properties;
 
 public class FastDFSClient {
 
     private TrackerClient trackerClient = null;
     private TrackerServer trackerServer = null;
 
-    public FastDFSClient() throws Exception {
-        String configPath = this.getClass().getClassLoader().getResource("fastdfs-client.properties").getFile();
-        System.out.println(configPath);
-        ClientGlobal.initByProperties(configPath);
+    public FastDFSClient(Properties properties) throws Exception {
 
+        ClientGlobal.initByProperties(properties);
         System.out.println("ClientGlobal.configInfo(): " + ClientGlobal.configInfo());
 
         trackerClient = new TrackerClient();
@@ -26,8 +25,24 @@ public class FastDFSClient {
         return new StorageClient1(trackerServer, trackerClient.getStoreStorage(trackerServer, groupName));
     }
 
-    public StorageClient1 getStorageClient1() throws Exception {
-        return new StorageClient1(trackerServer, trackerClient.getStoreStorage(trackerServer));
+    /**
+     * 得到Storage服务
+     *
+     * @param groupName
+     * @param storePathIndex
+     * @return
+     */
+    public StorageClient1 getStorageClient1(String groupName, int storePathIndex) {
+        try {
+            StorageServer defaultStoreStorage = trackerClient.getStoreStorage(trackerServer, groupName);
+            if (defaultStoreStorage != null) {
+                return new StorageClient1(trackerServer, new StorageServer(defaultStoreStorage.getInetSocketAddress().getAddress().getHostAddress(), defaultStoreStorage.getInetSocketAddress().getPort(), storePathIndex));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (MyException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
-
 }
